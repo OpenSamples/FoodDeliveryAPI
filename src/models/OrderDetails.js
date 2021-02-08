86c096e7d0e3599acf0e0b9b9affb21226a71617
ivanab <ivana@opticaltone.com>
2021-02-08 22:13:13 +0100

    Added Categories and OrderDetails models

---
+++ src/models/OrderDetails.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const OrderDetailsSchema = new mongoose.Schema(
+    {
+        totalAmount: {
+            type: Number,
+            required: true
+        },
+        products: [{
+            _id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'products',
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            },
+            qty: {
+                type: Number,
+                required: true
+            }
+        }],
+        orderId: {
+            type: Number,
+            ref: 'orders',
+            required: true
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('orderdetails', OrderDetailsSchema)
