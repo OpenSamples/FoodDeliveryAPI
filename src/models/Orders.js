0a59f4cbf93d5a098dca7cf620639d9f6790939e
ivanab <ivana@opticaltone.com>
2021-02-08 22:04:44 +0100

    Added Orders and ShoppingCartItems models

---
+++ src/models/Orders.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const OrdersSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        },
+        orderTotal: {
+            type: Number,
+            required: true
+        },
+        orderPlaced: {
+            type: Date,
+            required: true
+        },
+        isOrderCompleted: {
+            type: Boolean,
+            default: false
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'users',
+            required: true
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('orders', OrdersSchema)
