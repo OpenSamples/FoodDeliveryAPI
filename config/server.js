63043f17e990fbb3a92f6320e66f6416fb1679a3
Dino Karahmetovic <dino.prushka@gmail.com>
2021-02-12 00:03:01 +0100

    Included routes from ../src/routes/index

--- config/server.js
+++ config/server.js
@@ -3,16 +3,28 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+
+// We include all routes
+const {
+    accounts,
+    categories,
+    orders,
+    products,
+    shopping_cart_items,
+    users
+} = require('../src/routes')
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 //All routes
-app.use("/api/users", require("../routes/users"));
-app.use("/api/accounts", require("../routes/accounts"));
-app.use("/api/categories", require("../routes/categories"));
-app.use("/api/products", require("../routes/products"));
-app.use("/api/shopping-cart-items", require("../routes/shopping-cart-items"));
-app.use("/api/orders", require("../routes/orders"));
+app.use("/api/users", users);
+app.use("/api/accounts", accounts);
+app.use("/api/categories", categories);
+app.use("/api/products", products);
+app.use("/api/shopping-cart-items", shopping_cart_items);
+app.use("/api/orders", orders);
 
 
 module.exports = app
