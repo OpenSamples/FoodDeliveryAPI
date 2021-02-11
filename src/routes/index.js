82c4b972d4e1a7eeefac97ad551f10551a4d6b69
Dino Karahmetovic <dino.prushka@gmail.com>
2021-02-12 00:00:46 +0100

    Created index file for routes

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const accounts = require('./accounts')
+const categories = require('./categories')
+const orders = require('./orders')
+const products = require('./products')
+const shopping_cart_items = require('./shopping-cart-items')
+const users = require('./users')
+
+module.exports = {
+    accounts,
+    categories,
+    orders,
+    products,
+    shopping_cart_items,
+    users
+}
