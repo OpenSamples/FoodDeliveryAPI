d52e768bf39c4d631460307ca4dabe4518f339eb
Dino Karahmetovic <dino.prushka@gmail.com>
2021-02-13 19:47:15 +0100

    Added validation for order

--- src/validation/orderValidation.js
+++ src/validation/orderValidation.js
@@ -4,57 +4,56 @@ const Shopping_cart_items_model = require("../models/ShoppingCartItems");
 // Function to validate order
 async function validateOrder(orderData) {
     try {
-        const { userId, phone, address } = orderData;
-        const sci = await Shopping_cart_items_model.findOne({ userId: userId });
+        let requiredKeys = ['userId, phone, address']
         const errors = [];
-        const rePhone = /^\d{9}$/;
-        if (phone.length < 1 || address.length < 1) {
-            errors.push({ msg: "Must fill all fields." })
-        }
-        if (!rePhone.test(phone)) {
-            errors.push({ msg: "Invalid phone format." });
-        }
-        if (!sci) {
-            errors.push({ msg: "You must first fill Shopping Cart if you want to proceed with your order." });
+
+        for(let i = 0; i < requiredKeys.length; i++) {
+            let key = requiredKeys[i]
+
+            if(key in orderData && !orderData[key]) {
+                errors.push(`Invalid ${key}`)
+            }
+
+            if(!orderData[key]) {
+                errors.push(`${key} should be defined!`)
+            } else if(typeof orderData[key] !== 'string') {
+                errors.push(`${key} must be a string!`)
+            }
         }
 
+        if(errors.length) {
+            return {
+                error: !!errors.length,
+                message: 'Validation error!',
+                status: 406,
+                err_msg: errors
+            }
+        }
+
+        const { userId, phone, address } = orderData;
+        const sci = await Shopping_cart_items_model.findOne({ userId: userId });
+        const rePhone = /^\d{9}$/;
+
+        if (phone.length < 1 || address.length < 1) {
+            errors.push("Must fill all fields.")
+        }
+        if (!rePhone.test(phone)) {
+            errors.push("Invalid phone format.");
+        }
+        if (!sci) {
+            errors.push("You must first fill Shopping Cart if you want to proceed with your order.");
+        }
+
+
         return {
-            error: errors.length,
-            message: errors,
-            status: 406
+            error: !!errors.length,
+            message: 'Validation error!',
+            status: 406,
+            err_msg: errors
         }
     } catch(e) {
         return e
     }
 }
-
-
-//Same as userValidation,but here we are returning promise because we need to check if Shopping_cart_items with provided
-//userId exists if not we are pushing new error to errors and resolving it further
-
-// module.exports = (orderData) => {
-//     return new Promise(async (resolve, reject) => {
-//         try {
-//             const { userId, phone, address } = orderData;
-//             const sci = await Shopping_cart_items_model.findOne({ userId: userId });
-//             const errors = [];
-//             const rePhone = /^\d{9}$/;
-//             if (phone.length < 1 || address.length < 1) {
-//                 errors.push({ msg: "Must fill all fields." })
-//             }
-//             if (!rePhone.test(phone)) {
-//                 errors.push({ msg: "Invalid phone format." });
-//             }
-//             if (!sci) {
-//                 errors.push({ msg: "You must first fill Shopping Cart if you want to proceed with your order." });
-//             }
-//             resolve(errors);
-//         } catch (error) {
-//             console.log(error);
-//             reject(false);
-//         }
-//     });
-// };
-
 
 module.exports = validateOrder
