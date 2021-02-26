d9f45cc28b493dd333642c49fd393bb477e66ece
Dino Karahmetovic <dino.prushka@gmail.com>
2021-02-26 22:50:27 +0100

    Connecting with frontend

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -365,6 +365,38 @@ function getUserByEmail(email) {
 }
 
 
+function updatePassword(userId, password) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if( typeof password === 'string' 
+                && password.length > 4 
+                && password.length < 26 
+                && (/[a-z]/.test(password)) 
+                && (/[A-Z]/.test(password)) 
+                && (/[0-9]/.test(password))) {
+
+                    password = await bcrypt.hash(password, 10)
+
+                    resolve(Users.findOneAndUpdate({_id: userId}, {password}))
+                } else {
+                    reject({
+                        error: true,
+                        status: 401,
+                        message: 'Password is not valid!'
+                    })
+                }
+        } catch(e) {
+            reject({
+                error: true,
+                status: 500,
+                message: 'Something went wrong...',
+                err_msg: e
+            })
+        }
+    })
+}
+
+
 module.exports = {
     addUser,
     getAllUsers,
@@ -379,5 +411,6 @@ module.exports = {
     updateToken,
     clearToken,
     deleteById,
-    getUserByEmail
+    getUserByEmail,
+    updatePassword
 };
